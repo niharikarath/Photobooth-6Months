fb20d62cc955f7bda43a20e390d7a53a3b4eced0
Niha <136931504+niharikarath@users.noreply.github.com>
2025-12-09 04:31:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -145,28 +145,29 @@ if st.session_state.stage == "landing":
     # Photobooth Card Begin
     st.markdown('<div class="photobooth-card">', unsafe_allow_html=True)
 
-  # First two love lines (appear above or alongside images)
-st.markdown("""
-<div class="love-script love1">I can’t wait to kiss you in a photobooth one day</div>
-<div class="love-script love2">I love you so much, Aditya</div>
-""", unsafe_allow_html=True)
+  if st.session_state.stage == "landing":
 
-# Images
-st.markdown(f"""
-<img src="{img_to_datauri('1.png')}" style="width:160px; top:40px; left:-140px; transform:rotate(-5deg);" />
-<img src="{img_to_datauri('2.png')}" style="width:160px; top:20px; right:-140px; transform:rotate(5deg);" />
-<img src="{img_to_datauri('3.png')}" style="width:160px; top:180px; left:-160px; transform:rotate(-3deg);" />
-<img src="{img_to_datauri('4.png')}" style="width:160px; top:180px; right:-160px; transform:rotate(3deg);" />
-<img src="{img_to_datauri('5.png')}" style="width:160px; bottom:60px; left:-120px; transform:rotate(4deg);" />
-<img src="{img_to_datauri('6.png')}" style="width:160px; bottom:60px; right:-120px; transform:rotate(-4deg);" />
-""", unsafe_allow_html=True)
+    # First two love lines
+    st.markdown("""
+    <div class="love-script love1">I can’t wait to kiss you in a photobooth one day</div>
+    <div class="love-script love2">I love you so much, Aditya</div>
+    """, unsafe_allow_html=True)
 
-# Last two love lines (appear visually “after” images)
-st.markdown("""
-<div class="love-script love3">Best boyfriend</div>
-<div class="love-script love4">Happy 6 months, my love</div>
-""", unsafe_allow_html=True)
+    # Images
+    st.markdown(f"""
+    <img src="{img_to_datauri('1.png')}" style="width:160px; top:40px; left:-140px; transform:rotate(-5deg);" />
+    <img src="{img_to_datauri('2.png')}" style="width:160px; top:20px; right:-140px; transform:rotate(5deg);" />
+    <img src="{img_to_datauri('3.png')}" style="width:160px; top:180px; left:-160px; transform:rotate(-3deg);" />
+    <img src="{img_to_datauri('4.png')}" style="width:160px; top:180px; right:-160px; transform:rotate(3deg);" />
+    <img src="{img_to_datauri('5.png')}" style="width:160px; bottom:60px; left:-120px; transform:rotate(4deg);" />
+    <img src="{img_to_datauri('6.png')}" style="width:160px; bottom:60px; right:-120px; transform:rotate(-4deg);" />
+    """, unsafe_allow_html=True)
 
+    # Last two love lines (appear after images)
+    st.markdown("""
+    <div class="love-script love3">Best boyfriend</div>
+    <div class="love-script love4">Happy 6 months, my love</div>
+    """, unsafe_allow_html=True)
 
     # ---- Centered Enter Button ----
     st.markdown("""
@@ -324,6 +325,7 @@ elif st.session_state.stage == "done":
         st.error(f"Error creating the strip: {e}")
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
